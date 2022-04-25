4d493bf76ac7369c1702c11cd96a8e4508e4af5a
Cher Yang <yangyuxue2333@gmail.com>
2022-04-24 23:28:42 -0700

    updates
    fix bugs in simulation

--- script/RUN_simulation.py
+++ script/RUN_simulation.py
@@ -1,13 +1,16 @@
-from device_simon import *
+from simon_device import *
 import glob
 import itertools
 
+def check_parameters(param):
+	log = pd.read_csv("../data/log.csv", usecols=['motivation', 'init_cost', 'update_cost', 'valid_cue_percentage']).convert_dtypes()
+	logs = log.values
+	for l in logs:
+		if all(param == l):
+			return True
+	return False
+
 def simulate(): 
-    try:
-		log = pd.read_csv("../data/log.csv", usecols=['motivation', 'valid_cue_percentage', 'init_cost']).convert_dtypes()
-		ls = log[["motivation", "init_cost", "valid_cue_percentage"]].values
-	except:
-		ls = [] 
 	
 	motivation = np.linspace(0.1, 10, 10).round(2)
 	init_cost = np.linspace(0.01, 0.1, 10).round(2)
@@ -16,10 +19,10 @@ def simulate():
 
 	param_sets = list(itertools.product(*[motivation, init_cost, update_cost, valid_cue_percentage]))
 	for param in param_sets:
-		if check_parameters(param, ls):
+		if check_parameters(param):
 			print("SKIP", param)
 		else:
-			run_simulation(n=1, param_set={"motivation":param[0], "init_cost":param[1], 
+			run_simulation(n_simulation=1, n_session=7, param_set={"motivation":param[0], "init_cost":param[1],
                                            "update_cost":param[2], "valid_cue_percentage":param[3]})
             
-run_simulation(n_simulation=1, n_session=7, param_set={"init_cost":0.03, "update_cost":True, "valid_cue_percentage":.5, "motivation":1.5}, log=True)
+#run_simulation(n_simulation=1, n_session=7, param_set={"init_cost":0.03, "update_cost":True, "valid_cue_percentage":.5, "motivation":1.5}, log=True)
